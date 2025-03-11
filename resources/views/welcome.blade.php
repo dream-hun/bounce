3c422aed5076b2e11ed7a16723d4f60de778c7db
Dream Hunter <you@example.com>
2025-03-11 14:47:27 +0200

    added google tag manager

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,9 +10,31 @@
     <!-- Fonts -->
     <link rel="preload" href="https://rsms.me/inter/inter.css" as="style"
         onload="this.onload=null;this.rel='stylesheet'">
+    <!-- Google Tag Manager -->
+    <script>
+        (function(w, d, s, l, i) {
+            w[l] = w[l] || [];
+            w[l].push({
+                'gtm.start': new Date().getTime(),
+                event: 'gtm.js'
+            });
+            var f = d.getElementsByTagName(s)[0],
+                j = d.createElement(s),
+                dl = l != 'dataLayer' ? '&l=' + l : '';
+            j.async = true;
+            j.src =
+                'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+            f.parentNode.insertBefore(j, f);
+        })(window, document, 'script', 'dataLayer', 'GTM-PDDDZPJQ');
+    </script>
+    <!-- End Google Tag Manager -->
 </head>
 
 <body class="bg-black text-white">
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PDDDZPJQ" height="0" width="0"
+            style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
     <!-- Navigation Bar -->
     <x-header-component />
 
